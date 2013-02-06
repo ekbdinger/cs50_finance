80f8275e164683ad40962f666cea0ff94f655036
Paul <paulbosox@yahoo.com>
2013-02-05 20:05:01 -0500

    now getting an error for a blank username

--- html/register.php
+++ html/register.php
@@ -2,16 +2,50 @@
 
     // configuration
     require("../includes/config.php");
+    
+    // testing some code here
+    
+    if (empty($_POST["username"]))
+    {
+        apologize("please fill out a username");
+    }
 
     // if form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
         // TODO
+        
+        // inserts a user into the database
+        query("INSERT INTO users (username, hash, cash) VALUES(?, ?, 10000.00)", $_POST["username"], crypt($_POST["password"]));
+        
+        
+        
+         // check to see if the username is empty
+         // if password fields are empty
+         // if the password fields match
+        
+        // if $_POST["username"] or $_POST["password"] does not equal  $_POST["username"]
+        // from my ekb_account2.php files
+        // <?php if (empty($_POST["fname"]) || empty($_POST["lname"]) || empty($_POST["email"]) || empty($_POST["client"])      )
+        
+       
+        
+     
+        
     }
+    
+    // I'm assuming that this is basically if something goes wrong do this
     else
     {
         // else render form
         render("register_form.php", ["title" => "Register"]);
-    }
+    }  
+       /* if (empty($_POST["username"])
+            {
+            echo "Please fill out all of the fields.";
+            }
+            
+       */
+    
 
 ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 
             </div>
 
-            <div id="bottom">
+          <div id="bottom">
                 Copyright &#169; John Harvard
             </div>
 
